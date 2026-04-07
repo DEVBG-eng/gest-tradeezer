dff8f16fb8570ccd5c5205e0fc4307d2fb2ce5a2
Skip <skip@usecurling.com>
2026-04-07 16:36:21 +0000

    feat: Implement branded commercial budget view

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,4 +11,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// Add any other utility functions here
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value)
+}
